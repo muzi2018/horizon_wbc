a66bc2a4c7788273b8c49b9d47a3f281c1f25376
Weijie Wang <duangsir518@163.com>
2024-06-02 15:58:16 +0200

    back_ward

--- horizon_wbc.py
+++ horizon_wbc.py
@@ -74,12 +74,19 @@ ns = 0
 with open('/home/wang/horizon_wbc/output.txt', 'r') as file:
     lines = file.readlines()
 matrix = []
+global value 
 for line in lines:
     if index_ % 1 == 0: 
-        values = [float(x) for x in line.strip().split()]
-        matrix.append(values)
+        value = [float(x) for x in line.strip().split()]
+        matrix.append(value)
         ns = ns + 1
     index_ = index_ + 1
+
+for i in range(20):
+    value[0] -= i * 0.004
+    matrix.append(value)
+    ns = ns + 1
+
 ns = ns - 1
 
 T = 5.
@@ -299,24 +306,27 @@ msg = JointState()
 pub_sol = rospy.Publisher('pose_topic_sol', Pose, queue_size=1)
 pub_ref = rospy.Publisher('pose_topic_ref', Pose, queue_size=1)
 
-
+T_end = 4
+T = T_end
 while time <= T:
     # solution['q'][44,i] = 0.4 + i * 0.01
     # if solution['q'][44,i] >= 1.00:
         # solution['q'][44,i] = 1.00
     # solution['v'][43,i] = 0.0
+    solution['q'][44,i] = 0.0
+    solution['v'][43,i] = 0.0
     robot.setPositionReference(solution['q'][7:,i])
     robot.setVelocityReference(solution['v'][6:,i])
     robot.move() 
     i += 1
-    if i == 60:
-        closeDagana(pub_dagana)
+    # if i == 60:
+    #     closeDagana(pub_dagana)
     time += dt
     # if i >= 80:
     #     i = 80
     print("i = " , i)
-    # else:
-    
+    print("t = ", time)
+
     rate.sleep()
 exit()
 
